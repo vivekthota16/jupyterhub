3f01d5f5c0a6fe3f57b1281b080cfeec20ff861e
Vicky <vivekthota16@gmail.com>
2021-05-11 20:06:36 +0530

    updates on cuda,os,requirements

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 import os
-
+c.Spawner.environment = {}
 port = int(os.environ.get('JUPYTER_NOTEBOOK_PORT', '8080'))
 
 c.NotebookApp.ip = '0.0.0.0'
@@ -22,3 +22,12 @@ image_config_file = '/opt/app-root/src/.jupyter/jupyter_notebook_config.py'
 if os.path.exists(image_config_file):
     with open(image_config_file) as fp:
         exec(compile(fp.read(), image_config_file, 'exec'), globals())
+
+# Set Nvidia GPU configuration.
+
+c.Spawner.environment.update(dict(
+       NVIDIA_VISIBLE_DEVICES='all',
+       NVIDIA_DRIVER_CAPABILITIES='compute,utility',
+       NVIDIA_REQUIRE_CUDA='cuda>=8.0'))
+
+c.KubeSpawner.extra_resource_limits = {'nvidia.com/gpu': '1'}
